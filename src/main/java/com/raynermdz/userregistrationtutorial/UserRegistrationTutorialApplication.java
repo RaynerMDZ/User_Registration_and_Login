9a0803f0d42f7d4bf4391ec04a8f8e55c80dd64b
Rayner Mendez <raynermendezg@gmail.com>
2021-01-17 18:51:50 -0500

    Initial commit

---
+++ src/main/java/com/raynermdz/userregistrationtutorial/UserRegistrationTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.raynermdz.userregistrationtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationTutorialApplication.class, args);
+    }
+
+}
